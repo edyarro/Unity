a92fd8f3a6000fb36fbdd59e8bcd5d580385310c
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 21:18:35 -0500

    listen to keyboard input and add force to player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,10 +8,38 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody Rb;
 
     /// <summary>
+    /// forwardForce
+    /// </summary>
+    public float forwardForce = 2000f;
+
+    /// <summary>
+    /// sidewaysForce
+    /// </summary>
+    public float sidewaysForce = 500;
+
+    /// <summary>
     /// Update is called once per frame
     /// </summary>
     void FixedUpdate()
     {
-        this.Rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            this.Rb.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            this.Rb.AddForce(-this.sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            this.Rb.AddForce(0, 0, this.sidewaysForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            this.Rb.AddForce(0, 0, -this.sidewaysForce * Time.deltaTime);
+        }
     }
 }
