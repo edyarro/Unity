5aadb1654aa4846e6e376ed83084f6cdc077b9b7
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 22:02:47 -0500

    collision

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// PlayerCollision
+/// </summary>
+public class PlayerCollision : MonoBehaviour
+{
+    /// <summary>
+    /// movement
+    /// </summary>
+    public PlayerMovement movement;
+
+    /// <summary>
+    /// When two object with RigidBody and Collider collide
+    /// </summary>
+    /// <param name="collisionInfo"></param>
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
