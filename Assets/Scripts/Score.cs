53170ce75e0e64eb76cb8d2c06ac4bdf40dd21c9
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 23:47:36 -0500

    add some fonts show text on screen

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Score
+/// </summary>
+public class Score : MonoBehaviour
+{
+    /// <summary>
+    /// player
+    /// </summary>
+    public Transform player;
+
+    /// <summary>
+    /// scoreText
+    /// </summary>
+    public Text scoreText;
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        this.scoreText.text = this.player.position.z.ToString("0");
+    }
+}
