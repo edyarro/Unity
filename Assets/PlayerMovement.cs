a2d9061854a8ff4aa3ed4ee79efd44d3efb503e2
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 23:17:57 -0500

    add more obstable game

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,36 +10,33 @@ public class PlayerMovement : MonoBehaviour
     /// <summary>
     /// forwardForce
     /// </summary>
-    public float forwardForce = 2000f;
+    public float forwardForce;
 
     /// <summary>
     /// sidewaysForce
     /// </summary>
-    public float sidewaysForce = 500;
+    public float sidewaysForce;
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>
     void FixedUpdate()
     {
+        this.Rb.AddForce(0, 0, this.forwardForce * Time.deltaTime);
+
         if (Input.GetKey("d"))
         {
-            this.Rb.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0);
+            this.Rb.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a"))
         {
-            this.Rb.AddForce(-this.sidewaysForce * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey("w"))
-        {
-            this.Rb.AddForce(0, 0, this.sidewaysForce * Time.deltaTime);
+            this.Rb.AddForce(-this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("s"))
         {
-            this.Rb.AddForce(0, 0, -this.sidewaysForce * Time.deltaTime);
+            this.Rb.AddForce(0, 0, -this.sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
         }
     }
 }
