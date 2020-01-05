9203de63fd5605668a59353296a5decda8da8454
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 21:39:23 -0500

    add script for camera make the camera follow the player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// player
+    /// </summary>
+    public Transform player;
+
+    /// <summary>
+    /// offset
+    /// </summary>
+    public Vector3 offset;
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        this.transform.position = this.player.position + this.offset;
+    }
+}
