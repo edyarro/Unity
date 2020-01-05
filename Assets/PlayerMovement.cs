7dbdbd7f19885faffdbcde4c3945927074bc6763
Edwin Yarro <edwin.yarro@micromedica.com>
2020-01-04 20:20:13 -0500

    add script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("On start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
